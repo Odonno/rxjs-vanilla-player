6a2bbbd5f11b787a1331d11bd164d900e430ec8c
Odonno <bottiau.david@laposte.net>
2020-09-29 16:51:20 +0200

    feat: display basic timer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import './styles.css';
-import { sayHello } from "./greet";
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 const rootElement = document.getElementById("root");
+const startStopButton = document.getElementById("startStopButton");
+const outputElement = document.getElementById("output");
 
-if (rootElement) {
-    rootElement.innerText = sayHello();
+let currentIndex = 0;
+
+const displayRow = (value: any) => {
+    const date = new Date().toLocaleString();
+
+    if (outputElement) {
+        outputElement.innerHTML = `<div>#${currentIndex++}, Value: ${value}, Date: ${date}</div>` + outputElement.innerHTML;
+    }
+}
+
+if (rootElement && startStopButton && outputElement) {
+    // TODO : alternate start/stop button
+    // TODO : only one running observable
+    startStopButton.addEventListener("click", () => {
+        timer(0, 1000).pipe(
+            map(i => i ** 2)
+        ).subscribe(index => {
+            displayRow(index);
+        });
+    });
 }
